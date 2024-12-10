0c776c611f8b8f97cd01019729c16587f16f2cbb
shanujaiswal <shanuj4all@gmail.com>
2024-12-10 11:43:57 +0530

    third commit

--- javascript/clock.js
+++ javascript/clock.js
@@ -1,1 +1,21 @@
-console.log()
+function updateClock() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours() % 12;
+
+    const secondDegrees = (seconds / 60) * 360;
+    const minutesDegrees = ((minutes + seconds / 60) / 60) * 360;
+    const hoursDegrees = ((hours + minutes / 60) / 12) * 360;
+
+    document.querySelector('.second').style.transform =
+        `translateX(-50%) rotate(${secondDegrees}deg)`;
+    document.querySelector('.minute').style.transform =
+        `translateX(-50%) rotate(${minuteDegrees}deg)`;
+    document.querySelector('.hour').style.transform =
+        `translateX(-50%) rotate(${hourDegrees}deg)`;
+}
+setInterval(updateClock, 1000);
+
+updateClock();
