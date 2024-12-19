7a3cd1c99f29cb5fdc34c2ff2e33fbe87501f89f
shanujaiswal <shanuj4all@gmail.com>
2024-12-19 10:33:42 +0530

    first commit

--- javascript/Practice.js
+++ javascript/Practice.js
@@ -1,2 +1,18 @@
+"use strict";  // enables strict mode in JavaScript. When enabled, it enforces stricter parsing and error handling, helping you write cleaner and more secure code
+
+
 console.log("hello world");
 
+let firstname = "shanu";
+
+console.log(firstname);
+
+let firstName = "jaiswal"
+
+console.log(firstname+firstName);
+
+let value = 100;
+console.log(value *  11)
+let nam = "Shanu";
+ console.log(nam.length);
+ console.log(nam[nam.length-1])
