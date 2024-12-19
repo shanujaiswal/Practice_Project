fec6049563352d8c866de0abeb0b72a237fc690f
shanujaiswal <shanuj4all@gmail.com>
2024-12-19 11:18:19 +0530

    first commit

--- javascript/Practice.js
+++ javascript/Practice.js
@@ -1,18 +1,30 @@
 "use strict";  // enables strict mode in JavaScript. When enabled, it enforces stricter parsing and error handling, helping you write cleaner and more secure code
 
+//setting a value to variable
+let a = "shanu" 
+console.log(a);
 
+//printing words directly using console log
 console.log("hello world");
 
-let firstname = "shanu";
+// adding two variable 
+let b = "shanu";
+console.log(b);
+let c = "jaiswal"
+console.log(b + c);
 
-console.log(firstname);
+// Doing operation inside console 
+let d = 100;   
+console.log(d * 11)  //change signs for changing operation.
 
-let firstName = "jaiswal"
+//Finding lengths and words 
+let e = "Shanu";
+console.log(e.length); //find total length
+console.log(e[e.length - 1])  //find words from backward
 
-console.log(firstname+firstName);
-
-let value = 100;
-console.log(value *  11)
-let nam = "Shanu";
- console.log(nam.length);
- console.log(nam[nam.length-1])
+//Triming the words 
+let f = "  Shanu   e"
+f = f.trim(); // method 1 to trim 
+console.log(f)
+let h = f.trim();  //method 2  to trim 
+console.log(h);
