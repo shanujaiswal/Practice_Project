6a35cca50686ee68727423666eda36d955bf871d
shanujaiswal <shanuj4all@gmail.com>
2024-12-23 11:37:23 +0530

    first commit

--- javascript/codeWithHarshitDay1Part2.js
+++ javascript/codeWithHarshitDay1Part2.js
@@ -87,39 +87,39 @@ console.log("you are what")
 
 //nested if else
 
-let a = 49;
-let b = prompt("guess a number");
-if (b == a) {
+let p = 49;
+let q = prompt("guess a number");
+if (q == p) {
     console.log("you are idiot")
 } else {
-    if (b > a) {
+    if (q > p) {
         console.log("you are too much high")
-    } else (b < a)
+    } else (q < p)
     console.log("you are lowly hooman")
 }
 
-let c = 49;
-let d = Number(prompt("Guess a number")); // Convert input to number
+let r = 49;
+let s = Number(prompt("Guess a number")); // Convert input to number
 
-if (d === c) {
+if (s === r) {
     console.log("You got it right!");
-} else if (d < c - 40) {
+} else if (s < r - 40) {
     console.log("Way too low! Try a much higher number");
-} else if (d < c - 30) {
+} else if (s < r - 30) {
     console.log("Too low - go up by about 30+");
-} else if (d < c - 20) {
+} else if (s < r - 20) {
     console.log("Still low - go up by about 20+");
-} else if (d < c - 10) {
+} else if (s < r - 10) {
     console.log("Getting closer but still low - go up by about 10+");
-} else if (d > c + 40) {
+} else if (s > r + 40) {
     console.log("Way too high! Try a much lower number");
-} else if (d > c + 30) {
+} else if (s > r + 30) {
     console.log("Too high - go down by about 30+");
-} else if (d > c + 20) {
+} else if (s > r + 20) {
     console.log("Still high - go down by about 20+");
-} else if (d > c + 10) {
+} else if (s > r + 10) {
     console.log("Getting closer but still high - go down by about 10+");
-} else if (d > c) {
+} else if (s > r) {
     console.log("Almost there but still a bit high!");
 } else {
     console.log("Almost there but still a bit low!");
