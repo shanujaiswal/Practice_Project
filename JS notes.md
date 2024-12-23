4bc332370e9d3448d976d1b6fe4ad707c8819ae0
shanujaiswal <shanuj4all@gmail.com>
2024-12-23 14:59:09 +0530

    first commit

---
+++ JS notes.md
@@ -0,0 +1,5 @@
+Switch Statement -- evaluates a value against multiple cases and executes the code block of the matching case
+provide break if you want to stop the execution and no further responses from code otherwise it will provide all the given cases 
+
+
+in while loop 
--- javascript/codeWithHarshitDay1Part3.js
+++ javascript/codeWithHarshitDay1Part3.js
@@ -1,19 +1,76 @@
-//Switch Statement 
-
-let day = 0;
-if (day === 0) {
-    console.log("sunday")
-} else if (day === 1) {
-    console.log("sunday")
-} else if (day === 1) {
-    console.log("sunday")
-} else if (day === 1) {
-    console.log("sunday")
-} else if (day === 1) {
-    console.log("sunday")
-} else if (day === 1) {
-    console.log("sunday")
-} else if (day === 1) {
-    console.log("sunday")
+// Switch statement s
+//normal nested if else 
+let a = 0;
+if (a === 0) {
+    console.log("Sunday");
+} else if (a === 1) {
+    console.log("Monday");
+} else if (a === 2) {
+    console.log("Tueday");
+} else if (a === 3) {
+    console.log("Wednesday");
+} else if (a === 4) {
+    console.log("Thruday");
+} else if (a === 5) {
+    console.log("Friday");
+} else if (a === 6) {
+    console.log("Saturday");
+} else {
+    console.log("Invalid");
 }
 
+//Switch Statement -- evaluates a value against multiple cases and executes the code block of the matching case
+// provide break if you want to stop the execution and no further responses from code otherwise it will provide all the given cases 
+// we can run only one cases at a time 
+let b = 2;
+switch (b) {
+    case 0:
+        console.log("Sunday");
+        break;
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tueday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thruday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Invalid Day");
+        break;
+}
+
+// while loop
+let c = 0;
+while (c <= 7) {
+    console.log(c)
+    c++
+}
+
+//method 1 
+let d = 0
+let e = 0
+while (e<=100){
+    d = d + e;
+    e++;
+}
+console.log(d)
+
+//method 2
+let f =100;
+ let g = (f*(f+1))/2;
+ console.log(g)
+
+ // for loop 
+
+ for(let )
