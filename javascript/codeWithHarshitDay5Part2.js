33453f03cead48819d5203efa4a4249468c0a7cd
shanujaiswal <shanuj4all@gmail.com>
2025-01-29 12:00:42 +0530

    first commit

---
+++ javascript/codeWithHarshitDay5Part2.js
@@ -0,0 +1,7 @@
+const a = new Map()
+a.set('firstname',"Shanu");
+a.set('age',7);
+a.set(1,'age');
+console.log(a)
+console.log(a.get('firstname'))       // Retrieve and log the value associated with the key 'firstname' from the Map
+console.log(a.keys())       // Retrieve and log the value associated with the key 'firstname' from the Map
