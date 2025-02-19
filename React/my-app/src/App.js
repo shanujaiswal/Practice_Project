683c4aa88942206607c2d15b8658fcd366901a91
Shanu Jaiswal <shanuj4all@gmail.com>
2025-02-19 17:23:12 +0530

    first commit

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -5,8 +5,10 @@ import TextForm from "./components/TextForm";
 function App() {
   return (
     <>
-      <Navbar title="Shanu" text1="SuperAdmin" text2="Org Admin"  />
-      <TextForm/>
+      <Navbar title="Shanu" text1="SuperAdmin" text2="Org Admin" />
+      <div className="container my-3">
+        <TextForm heading="Enter the Text"/>
+      </div>
     </>
   );
 }
--- React/my-app/src/components/TextForm.js
+++ React/my-app/src/components/TextForm.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-export default function TextForm() {
+export default function TextForm(props) {
   return (
-    <div><div class="mb-3">
-    <label for="exampleFormControlInput1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-  </div>
-  <div class="mb-3">
-    <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div></div>
+    <div>
+      <h1>{props.heading}</h1>
+     <div class="mb-3">
+       {/* <label for="myBox" class="form-label">Example TextArea</label> */}
+        <textarea classnm jk  nh kh Name="form-control" id="myBox" rows="8"></textarea>
+      </div>
+      <button className="btn btn-primary">Convert to Uppercase </button>
+    </div>
   )
 }
